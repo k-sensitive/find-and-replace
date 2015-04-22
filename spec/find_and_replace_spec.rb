ae272b4984c83e8ef4ee60cd4feb77b8153c9531
Courtney Phillips & Kim Dang <student@epicodus.com>
2015-04-22 16:08:30 -0700

    Tests for capitalization compatibility.

--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -9,4 +9,16 @@ describe('String#find_and_replace') do
   it("replaces a specific word with another in a phrase") do
     expect("hello world".find_and_replace("world", "universe")).to(eq("hello universe"))
   end
+
+  it("replaces a word and handles capitalization") do
+    expect("MeGaTrOn".find_and_replace("megatron", "doilies")).to(eq("doilies"))
+  end
+
+  it("replaces a word and handles capitalization") do
+    expect("Hello WoRld".find_and_replace("world", "universe")).to(eq("hello universe"))
+  end
+  # it("replaces a partial matches in larger words") do
+  #   expect("cathedral".find_and_replace("cat", "fish")).to(eq("fishthedral"))
+  # end
+
 end
