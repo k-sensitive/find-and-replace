df864ba3317802ef3d1264fa0592527024fb9124
Courtney Phillips & Kim Dang <student@epicodus.com>
2015-04-22 15:50:27 -0700

    Replaces individual word in a phrase.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,7 +1,14 @@
 class String
   define_method(:find_and_replace) do |original, replace|
-    if self == original
-      replace
+    phrase = self.split()
+    new_array = []
+    phrase.each() do |word|
+      if word == original
+        new_array.push(replace)
+      else
+        new_array.push(word)
+      end
     end
+    new_array.join(" ")
   end
 end
