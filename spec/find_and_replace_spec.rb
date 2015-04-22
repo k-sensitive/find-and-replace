1d3c3c9e335d6c0a36692cc46d308ac9b62916b1
Courtney Phillips & Kim Dang <student@epicodus.com>
2015-04-22 15:26:07 -0700

    Add word replace for phrase test.

--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -1,8 +1,12 @@
 require('rspec')
-require('./lib/find_and_replace')
+require('find_and_replace')
 
 describe('String#find_and_replace') do
   it("replaces one word with another") do
-    expect("megatron".find_and_replace("megatron", "doilies").to(eq("doilies"))
+    expect("megatron".find_and_replace("megatron", "doilies")).to(eq("doilies"))
+  end
+
+  it("replaces a specific word with another in a phrase") do
+    expect("hello world".find_and_replace("world", "universe")).to(eq("hello universe"))
   end
 end
