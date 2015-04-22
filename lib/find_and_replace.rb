d75fac07b20e2523b5e39cf4f677112e505eac96
Courtney Phillips & Kim Dang <student@epicodus.com>
2015-04-22 16:34:25 -0700

    Utilize parititon method for partial word replacement.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,17 +1,15 @@
 class String
   define_method(:find_and_replace) do |original, replace|
-    phrase = self.split()
+    self.downcase!()
     new_array = []
-    phrase.each() do |word|
-      word.downcase!()
+    broken_phrase = self.partition(original)
+    broken_phrase.each() do |word|
       if word == original
         new_array.push(replace)
-      # elsif word.titlecase?() == true
-      #   new_array.push(replace)
       else
         new_array.push(word)
       end
     end
-    new_array.join(" ")
+    new_array.join()
   end
 end
